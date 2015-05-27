9952e0ee431370b536b1272899f88661d0516afa
Jacob Gray <jacob@jacob-gray.net>
2015-05-27 00:26:01 -0400

    week9

--- week5/lab/api/v1/models/helpers/RestProxy.php
+++ week5/lab/api/v1/models/helpers/RestProxy.php
@@ -24,7 +24,6 @@ class RestProxy implements IService {
             500 => 'Internal Server Error',
         );
      
-        
     protected function setHeaders($status = 200) {
         header("Access-Control-Allow-Orgin: *");
         header("Access-Control-Allow-Methods: *");
